005639a21bda157d065535e9dd743cc4d3145c3e
jacksonreismelo <jacksonreismelo@gmail.com>
2025-03-20 16:54:28 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', '15.235.9.156');
+define('DB_PORT', '3306');
+define('DB_DATABASE', 'jacks4995_abc_conexao');
+define('DB_USERNAME', 'jacks4995_admin');
+define('DB_PASSWORD', 'vm102030!');
